67bc850e84a2c0416271d9ed5c66311e82e8c7fb
Diogo Gouveia <diogocg.dev@gmail.com>
2022-02-09 19:12:25 -0300

    Requisito 4

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CategoriesList from './CategoriesList';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <CategoriesList />
         <Link to="/cart" data-testid="shopping-cart-button">carrinho</Link>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
