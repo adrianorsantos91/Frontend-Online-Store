e16d298c71da7cac2460a7c7eb37ecbf9765b4b7
Diogo Gouveia <diogocg.dev@gmail.com>
2022-02-09 16:03:54 -0300

    Requisito 1 ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div />
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div />
+    );
+  }
 }
 
 export default App;
