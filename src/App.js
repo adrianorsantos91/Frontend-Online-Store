8fde6395fab8e03df0d2c0181339176f5b5a1146
Diogo Gouveia <diogocg.dev@gmail.com>
2022-02-09 17:36:00 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Cart from './components/Cart';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
+          <Route exact path="/cart" component={ Cart } />
         </Switch>
       </BrowserRouter>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <Link to="/cart" data-testid="shopping-cart-button">carrinho</Link>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
