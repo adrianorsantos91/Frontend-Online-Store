63ccfeb937d2bfcd6ca54f4da310be03e2cc4a51
joao <joao.pedro.ramos@outlook.com>
2022-02-14 17:48:29 +0000

    mais erros

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Cart from './components/Cart';
