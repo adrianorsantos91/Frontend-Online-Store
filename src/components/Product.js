4b31692de6af0faf52f2b5928a1cf427d6226b16
Adriano Santos <adriano.santosars@outlook.com>
2022-02-14 23:56:16 -0300

    Requisito 11 - em andamento

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,21 +13,43 @@ class Product extends React.Component {
       attributes: [],
       email: '',
       comment: '',
+      note: '',
+      NUMs: [],
       avaliation: [],
+      avaliationLocal: [{
+        email: '',
+        note: '',
+        comment: '',
+      }],
     };
   }
 
   componentDidMount = () => {
     this.getProducts();
+    // this.getLocalStorage();
+  }
+
+  getLocalStorage = () => {
+    const avaliation = localStorage.getItem('avaliation');
+    const result = JSON.parse(avaliation);
+    this.setState({
+      avaliationLocal: result,
+    });
+  }
+
+  updateLocalStorage = () => {
+    const { avaliation } = this.state;
+    localStorage.setItem('avaliation', JSON.stringify(avaliation));
+    this.getLocalStorage();
   }
 
   sendToLocal = () => {
-    const { email, comment, avaliation } = this.state;
+    const { email, comment, note, avaliation } = this.state;
     this.setState({
-      avaliation: [...avaliation, { email, comment }],
-    }, () => console.log(avaliation));
-    localStorage.setItem('email', email);
-    localStorage.setItem('comment', comment);
+      avaliation: [...avaliation, { email, comment, note }],
+    }, () => this.updateLocalStorage());
+
+    // localStorage.setItem('comment', comment);
     // const result = localStorage.getItem('email');
   }
 
@@ -38,17 +60,25 @@ class Product extends React.Component {
   getProducts = async () => {
     const { match } = this.props;
     const productInfo = await api.getProductsFromId(match.params.id);
+    const NUM1 = 1;
+    const NUM2 = 2;
+    const NUM3 = 3;
+    const NUM4 = 4;
+    const NUM5 = 5;
+    const NUMs = [NUM1, NUM2, NUM3, NUM4, NUM5];
     this.setState({
       title: productInfo.title,
       price: productInfo.price,
       thumbnail: productInfo.thumbnail,
       attributes: productInfo.attributes,
+      NUMs,
     });
   }
 
   render() {
-    const { title, price, thumbnail, attributes } = this.state;
+    const { title, price, thumbnail, attributes, avaliationLocal, NUMs } = this.state;
     const { onClickButton, match } = this.props;
+
     return (
       <section>
         <div>
@@ -59,32 +89,6 @@ class Product extends React.Component {
             Carrinho
           </Link>
         </div>
-
-        <p>Avaliações</p>
-        <form>
-          <input
-            name="email"
-            placeholder="Email"
-            type="email"
-            data-testid="product-detail-email"
-            onChange={ this.handleInputText }
-          />
-          <br />
-          <textarea
-            name="comment"
-            placeholder="Comentário sobre o produto. (opcional)"
-            data-testid="product-detail-evaluation"
-            onChange={ this.handleInputText }
-          />
-          <button
-            type="button"
-            data-testid="submit-review-btn"
-            onClick={ this.sendToLocal }
-          >
-            Enviar
-          </button>
-        </form>
-
         <div>
           <p data-testid="product-detail-name">{ title }</p>
           <img src={ thumbnail } alt={ title } />
@@ -114,6 +118,63 @@ class Product extends React.Component {
             </div>
           ))}
         </aside>
+        <div>
+          <br />
+          <p>Avaliações</p>
+          <form>
+            <input
+              name="email"
+              placeholder="Email"
+              type="email"
+              data-testid="product-detail-email"
+              onChange={ this.handleInputText }
+            />
+            <div>
+              <br />
+              { NUMs.map((NUM) => (
+                <label key={ NUM } htmlFor={ `option${NUM}` } control>
+                  <input
+                    name="note"
+                    id={ `option${NUM}` }
+                    type="radio"
+                    value={ NUM }
+                    data-testid={ `${NUM}-rating` }
+                    onChange={ this.handleInputText }
+                  />
+                  {' '}
+                  { NUM }
+                </label>)) }
+            </div>
+            <br />
+            <textarea
+              name="comment"
+              placeholder="Comentário sobre o produto. (opcional)"
+              data-testid="product-detail-evaluation"
+              onChange={ this.handleInputText }
+            />
+            <br />
+            <button
+              type="button"
+              data-testid="submit-review-btn"
+              onClick={ this.sendToLocal }
+            >
+              Enviar
+            </button>
+          </form>
+        </div>
+        <div>
+          { avaliationLocal.length === 0
+            ? (
+              <p>Não possui Avaliação</p>
+            ) : (
+              avaliationLocal.map(({ email, comment, note }) => (
+                <div key={ email }>
+                  <p>{ email }</p>
+                  <p>{ note }</p>
+                  <p>{ comment }</p>
+                </div>
+              )))}
+        </div>
       </section>
     );
   }
